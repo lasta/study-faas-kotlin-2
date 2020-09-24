4029604693e5c3344d896d8485402c4d345eb49f
Atsushi KOSHIKIZAWA <lastarrow21@gmail.com>
2020-09-24 21:06:35 +0900

    succeeded to switch target automatically

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,16 +8,15 @@ repositories {
     mavenCentral()
 }
 kotlin {
+    // TODO: cross-compile for platforms at once
     val hostOs = System.getProperty("os.name")
     val isMingwX64 = hostOs.startsWith("Windows")
-//    val nativeTarget = when {
-//        hostOs == "Mac OS X" -> macosX64("native")
-//        hostOs == "Linux" -> linuxX64("native")
-//        isMingwX64 -> mingwX64("native")
-//        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-//    }
-
-    val nativeTarget = linuxX64("native")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
 
     nativeTarget.apply {
         binaries {
@@ -26,6 +25,7 @@ kotlin {
             }
         }
     }
+
     sourceSets {
         @kotlin.Suppress("UNUSED_VARIABLE")
         val nativeMain by getting
