fd5792aeb9e8bf96d252ddf900b5283a5b1fa0db
Atsushi KOSHIKIZAWA <lastarrow21@gmail.com>
2020-09-25 23:30:39 +0900

    upgrade kotlin from 1.4.0 to 1.4.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.4.0"
+    kotlin("multiplatform") version "1.4.10"
 }
 group = "me.lasta"
 version = "1.0-SNAPSHOT"
--- buildSrc/src/main/kotlin/entrypoint/EntryPoint.kt
+++ buildSrc/src/main/kotlin/entrypoint/EntryPoint.kt
@@ -3,7 +3,7 @@ package entrypoint
 data class EntryPoint(
     val packageName: String,
     val functionName: String = "main",
-    val args: Array<String>? = null
+    val args: Array<String> = emptyArray()
 ) {
     val entryFunction: String
         get() = "$packageName.$functionName"
@@ -15,22 +15,13 @@ data class EntryPoint(
         other as EntryPoint
 
         if (packageName != other.packageName) return false
-        if (functionName != other.functionName) return false
-        if (args != null) {
-            if (other.args == null) return false
-            if (!args.contentEquals(other.args)) return false
-        } else if (other.args != null) return false
 
         return true
     }
 
     override fun hashCode(): Int {
-        var result = packageName.hashCode()
-        result = 31 * result + functionName.hashCode()
-        result = 31 * result + (args?.contentHashCode() ?: 0)
-        return result
+        return packageName.hashCode()
     }
-
 }
 
 val ENTRY_POINTS: List<EntryPoint> = listOf(
