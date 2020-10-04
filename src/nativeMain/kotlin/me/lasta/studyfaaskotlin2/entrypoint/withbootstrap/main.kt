192f9591f580e409c9e176a26963b854022b1ee5
Atsushi KOSHIKIZAWA <lastarrow21@gmail.com>
2020-10-04 23:38:26 +0900

    Revert "refactored"
    This reverts commit b6226c15a47168af84e241e78958ba4826c67292.

--- src/nativeMain/kotlin/me/lasta/studyfaaskotlin2/entrypoint/withbootstrap/main.kt
+++ src/nativeMain/kotlin/me/lasta/studyfaaskotlin2/entrypoint/withbootstrap/main.kt
@@ -6,21 +6,27 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.util.*
 import io.ktor.utils.io.core.*
+import kotlinx.cinterop.toKString
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import me.lasta.studyfaaskotlin2.lambda.CustomRuntime
-import me.lasta.studyfaaskotlin2.lambda.RuntimeEnv
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import platform.posix.getenv
 
 @KtorExperimentalAPI
 fun main() = runBlocking {
-    lateinit var lambdaEnv: RuntimeEnv
-    val httpClient = HttpClient(Curl)
-    val customRuntime = CustomRuntime(client = httpClient)
+    println("hello withbootstrap")
+    lateinit var lambdaEnv: LambdaCustomRuntimeEnv
 
     try {
+        // initialize
+        val lambdaRuntimeApi = requireNotNull(getenv("AWS_LAMBDA_RUNTIME_API")).toKString()
+
         bootstrapLoop@ while (true) {
             // preprocessing each request
-            lambdaEnv = customRuntime.initialize()
+            lambdaEnv = LambdaCustomRuntime.initialize()
 
             // business logic
             val userArticle: String = HttpClient(Curl).use { client ->
@@ -31,18 +37,125 @@ fun main() = runBlocking {
                     // Application error each request
                     println("Application Error")
                     e.printStackTrace()
-                    customRuntime.sendInvocationError(lambdaEnv, e)
+                    LambdaCustomRuntime.sendInvocationError(lambdaEnv, e)
                     null
                 }
             } ?: continue
             // postprocessing each request
-            customRuntime.sendResponse(lambdaEnv, userArticle)
+            LambdaCustomRuntime.sendResponse(lambdaEnv, userArticle)
         }
     } catch (e: Exception) {
         // Initialization error
         print("Initialization Error")
         e.printStackTrace()
-        customRuntime.sendInitializeError(lambdaEnv, e)
+        LambdaCustomRuntime.sendInitializeError(lambdaEnv, e)
     }
 }
 
+@Serializable
+data class UserArticle(
+    val userId: Int,
+    val id: Int,
+    val title: String,
+    val body: String
+)
+
+object LambdaCustomRuntime {
+    val lambdaRuntimeApi = requireNotNull(getenv("AWS_LAMBDA_RUNTIME_API")).toKString()
+
+    @KtorExperimentalAPI
+    suspend fun initialize(): LambdaCustomRuntimeEnv = HttpClient(Curl).use { client ->
+        try {
+            LambdaCustomRuntimeEnv(client.get("http://$lambdaRuntimeApi/2018-06-01/runtime/invocation/next"))
+        } catch (e: Exception) {
+            TODO("do something from initialize")
+        }
+    }
+
+    @KtorExperimentalAPI
+    suspend fun sendInvocationError(lambdaEnv: LambdaCustomRuntimeEnv, error: Exception) {
+        HttpClient(Curl).use { client ->
+            val proc: HttpResponse = try {
+                client.post {
+                    url("http://$lambdaRuntimeApi/2018-06-01/runtime/invocation/${lambdaEnv.requestId}/error")
+                    body = Json.encodeToString(
+                        mapOf(
+                            "errorMessage" to error.toString(),
+                            "errorType" to "InvocationError"
+                        )
+                    )
+                }
+            } catch (e: Exception) {
+                TODO("do something from send invocation error")
+            }
+
+            println(proc.toString())
+        }
+    }
+
+    @KtorExperimentalAPI
+    suspend fun sendInitializeError(lambdaEnv: LambdaCustomRuntimeEnv, error: Exception) {
+        HttpClient(Curl).use { client ->
+            val proc: HttpResponse = try {
+                client.post {
+                    url("http://$lambdaRuntimeApi/2018-06-01/runtime/init/error")
+                    body = Json.encodeToString(
+                        mapOf(
+                            "errorMessage" to error.toString(),
+                            "errorType" to "InvocationError"
+                        )
+                    )
+                }
+            } catch (e: Exception) {
+                TODO("do something from send initialize error")
+            }
+
+            println(proc.toString())
+        }
+    }
+
+    @KtorExperimentalAPI
+    suspend inline fun sendResponse(lambdaEnv: LambdaCustomRuntimeEnv, response: String) {
+        HttpClient(Curl).use { client ->
+            val proc: HttpResponse = try {
+                client.post {
+                    url("http://$lambdaRuntimeApi/2018-06-01/runtime/invocation/${lambdaEnv.requestId}/response")
+                    body = Json.encodeToString(ResponseMessage(body = response))
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                TODO("do something from send response")
+            }
+            println(proc.toString())
+        }
+    }
+}
+
+@Serializable
+data class ResponseMessage(
+    val statusCode: Int = 200,
+    val body: String
+)
+
+class LambdaCustomRuntimeEnv(
+    private val response: HttpResponse
+) {
+    private val body: LambdaRuntimeRequestBody
+        // TODO change body data class
+        get() = Json.decodeFromString(response.toString())
+
+    val requestId: String
+        get() = requireNotNull(
+            response.headers["lambda-runtime-aws-request-id"],
+            lazyMessage = { "\"lambda-runtime-aws-request-id\" must not be null" }
+        )
+
+    // FIXME: define request value in LambdaRuntimeRequestBody and delete this function
+    fun getRequestParameter(key: String) = body.parameters[key]
+}
+
+@Serializable
+data class LambdaRuntimeRequestBody(
+    @SerialName("queryStringParameters")
+    val parameters: Map<String, String>
+)
